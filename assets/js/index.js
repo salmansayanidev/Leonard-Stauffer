277b33d288d2a7bf06cd1c1f9186c015a612e07e
salmansayani <salmansayani822@gmail.com>
2024-06-12 06:42:39 +0500

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+$(".bnner-slider-area").slick({
+  infinite: true,
+  slidesToShow: 1,
+  arrows: true,
+  slidesToScroll: 1,
+  nextArrow: $(".bnner-slider-btn.next"),
+  prevArrow: $(".bnner-slider-btn.pre"),
+});
